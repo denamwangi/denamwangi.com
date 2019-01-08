14fe349c5bd26203c3901fe39c914d8048beafb3
denamwangi <dena@getsentry.com>
2019-01-07 21:37:30 -0800

    formatting r1

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,21 +4,19 @@ import HeaderLink from './headerLink';
 
 class Header extends Component {
 
-  render() {
-  	const sections = ['about', 'experience', 'education', 'talks', 'contact']
+	render() {
+  	const sections = ['about', 'experience', 'education', 'talks', 'contact'];
   	return (
-		      
-        <header className="App-header">
-	        <ul>
-				{sections.map(section => (
-					<li className="nav-item">
-						<HeaderLink name={section}/>
-					</li>
-				))}
-	        </ul>
-        </header>
-  	)
-  }
-};
+	  <header className="App-header">
+	    <ul>
+		  {sections.map(section => (
+		    <li className="nav-item">
+							<HeaderLink name={section}/>
+						</li>
+		  ))}
+	    </ul>
+	  </header>
+  	);
+	}}
 
 export default Header;
